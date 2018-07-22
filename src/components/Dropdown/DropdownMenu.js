d1787fa67fb4093d67a463b1e47c5e8486ce1607
mattcraigfleming <mfleming@zonedigital.com>
2018-07-22 14:46:19 +0100

    Refactor dropdown, added styling, styled-components

--- src/components/Dropdown/DropdownMenu.js
+++ src/components/Dropdown/DropdownMenu.js
@@ -1,14 +1,81 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default class DropdownMenu extends React.Component {
-    render() {
-      let className="dropdown-select";
-      if (this.props.toggled) {
-        className += " open";
+class DropdownMenu extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ""
+    }
+    this.handleMenuClick = this.handleMenuClick.bind(this)
+  }
+
+  handleMenuClick(e) {
+    console.log(e.target.value)
+  }
+  render() {
+    const { className, data } = this.props;
+    return (
+     
+      <div className={className}>
+        <ul className="dropdown">
+          {data.map(item => {
+            const { title } = item;
+            return (
+
+                  <li key={title} onClick={this.handleMenuClick} value={title}>{title}</li>
+
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+
+}
+
+export default styled(DropdownMenu)`
+  .dropdown {
+    display: flex;
+    justify-content: space-between;
+    outline: none;
+    visibility: hidden;
+    transition: visibility 0.5s;
+    top: 100%;
+    position: relative;
+    right: 140px;
+    min-width: 80px;
+    padding: 5px 0;
+    margin: 2px 0 0;
+    font-size: 14px;
+    flex-direction: column;
+    align-items: center;
+    list-style: none;
+    background-color: #fff;
+    -webkit-background-clip: padding-box;
+    background-clip: padding-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+    box-shadow: 0 6px 12px rgba(0,0,0,.175);
+  }
+  .dropdown__item {
+    cursor: pointer;
+    text-align: left;
+    line-height: 24px;
+    font-size: 14px;
+    outline: none;
+    
+    a {
+      margin: 8px 16px;
+      text-decoration: none ;
+      display: block;
+      span {
+        white-space: nowrap;
+        overflow: hidden;
+        color: #4a4d55;
       }
-      
-      return <ul className={className}>
-            {this.props.children}
-          </ul>
     }
   }
+`;
---
+++ src/components/Dropdown/menuData.js
@@ -0,0 +1,22 @@
+export const menuData = [
+    {
+        title: 'USD',
+        classNames: 'dropdown__item'
+    },
+    {
+        title: 'GBP',
+        classNames: 'dropdown__item'
+    },
+    {
+        title: 'EUR',
+        classNames: 'dropdown__item'
+    },
+    {
+        title: 'JPY',
+        classNames: 'dropdown__item'
+    },
+    {
+        title: 'KRW',
+        classNames: 'dropdown__item'
+    },
+]
